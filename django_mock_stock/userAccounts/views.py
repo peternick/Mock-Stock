1db23c72d1bb65ee12fc230b5a4d70a26e0b14dc
Peter Apazidis <peternick2456@gmail.com>
2021-06-03 10:32:59 -0400

    Added some comments to make code logic more clear

--- django_mock_stock/userAccounts/views.py
+++ django_mock_stock/userAccounts/views.py
@@ -3,14 +3,14 @@ from .forms import AccountForm
 from .models import Account
 
 def login(request):
-    bool_create_account = 'false'
+    bool_create_account = 'false' #sent to the login page to determine whether a new account will be created or not
     if request.method == 'POST':
         if request.POST.__contains__('create_accnt'):
             bool_create_account = 'true'
         else:
             form = AccountForm(request.POST or None)
             if form.is_valid():
-                form.save()
+                form.save() 
                 bool_create_account = 'false'
                 
     context = {'bool_create_account': bool_create_account}
