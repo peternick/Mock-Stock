b0fbec0bdf9e38b0cbde637be614cfd604630a2c
Peter Apazidis <peternick2456@gmail.com>
2021-06-03 09:22:24 -0400

    Creating an account now works correctly, where a given username and password will be saved to the database along with an initial balance of 10000 dollars. Utilized Django's conditional expressions in login.html to make the page more dynamic and use less code. This allows for the login page to act as a login page and an account creation page.

--- django_mock_stock/userAccounts/views.py
+++ django_mock_stock/userAccounts/views.py
@@ -3,10 +3,17 @@ from .forms import AccountForm
 from .models import Account
 
 def login(request):
+    bool_create_account = 'false'
     if request.method == 'POST':
-        form = AccountForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-    context = {'bool_create_account': 'false'}
+        if request.POST.__contains__('create_accnt'):
+            bool_create_account = 'true'
+        else:
+            form = AccountForm(request.POST or None)
+            if form.is_valid():
+                form.save()
+                bool_create_account = 'false'
+                
+    context = {'bool_create_account': bool_create_account}
     return render(request, 'login.html', context)
-# Create your views here.
+
+
