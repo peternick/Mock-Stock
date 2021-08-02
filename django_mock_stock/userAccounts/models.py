b4cf57c1c6df9ae629df83c780b41acd0ba7c1d4
Peter Apazidis <peternick2456@gmail.com>
2021-08-01 22:55:44 -0400

    Added a new model to the database containing data on a stock

--- django_mock_stock/userAccounts/models.py
+++ django_mock_stock/userAccounts/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+
+class Stock(models.Model):
+    stock_ticker = models.CharField(max_length=6)
+    company_name = models.CharField(max_length=40)
+    number_stocks = models.PositiveIntegerField()
+    total_value = models.DecimalField(decimal_places=2, max_digits=12)
+
+    def __str__(self) -> str:
+        return self.stock_ticker
+
+
 class Account(models.Model):
     username = models.CharField(max_length=30)
     password = models.CharField(max_length=20)
-    balance = models.DecimalField(decimal_places=2, max_digits=9)
+    balance = models.DecimalField(decimal_places=2, max_digits=12)
+    owned_stocks = models.ManyToManyField(Stock, db_table='stock_table', blank=True)
 
     def __str__(self) -> str:
         return self.username
     
-
